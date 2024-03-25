9bbc7852f4ec5b97e05ba3441713d9c5cabf41ee
Hugh <ygz14835187@163.com>
2024-03-25 17:25:08 +0800

    update version

--- components/UpdateVersionModal.tsx
+++ components/UpdateVersionModal.tsx
@@ -15,34 +15,34 @@ import useSWR from "swr";
 
 export const UpdateVersionModal = () => {
   const [open, setOpen] = useState(false);
-  const { data: appVersion } = useSWR(`getAppVersion`, () => getAppVersion(), {
-    refreshInterval: 1000 * 60 * 2,
-  });
-  const timer = useRef<any>();
   const showModal = () => {
     setOpen(true);
   };
+  useSWR(`getAppVersion`, () => getAppVersion(), {
+    refreshInterval: 1000 * 60 * 2,
+    onSuccess: (appVersion) => {
+      console.log(appVersion);
+      if (appVersion && Number(appVersion) > version) {
+        showModal();
+      }
+    },
+  });
+  const timer = useRef<any>();
+
   const refresh = () => {
     setOpen(false);
     window.location.reload();
   };
-  const checkVersion = () => {
-    console.log("appVersion", appVersion);
-    console.log("version", version);
-    if (appVersion && Number(appVersion) > version) {
-      showModal();
-    }
-  };
   const hideModal = () => {
     if (timer.current) clearTimeout(timer.current);
-    timer.current = setTimeout(() => {
-      checkVersion();
-    }, 1000 * 60 * 5);
+    timer.current = setTimeout(
+      () => {
+        // checkVersion();
+      },
+      1000 * 60 * 5,
+    );
     setOpen(false);
   };
-  useEffect(() => {
-    checkVersion();
-  }, [appVersion]);
   return (
     <Modal
       backdrop="blur"
@@ -54,7 +54,7 @@ export const UpdateVersionModal = () => {
         <ModalHeader className="">有新版本，是否更新？</ModalHeader>
         <ModalFooter>
           <Button color="primary" variant="light" onPress={hideModal}>
-            取消
+            稍后再说
           </Button>
           <Button color="danger" onPress={refresh}>
             立即更新
