542787391a4ec7e41ac976ea1df2239fa3799024
duyouyou <duyouyou88@gmail.com>
2024-05-13 22:56:03 +0800

    change the size of the order list

--- components/order/OrdxOrderList.tsx
+++ components/order/OrdxOrderList.tsx
@@ -42,7 +42,7 @@ export const OrdxOrderList = ({
   const [buyItem, setBuyItem] = useState<any>();
   const [orderRaw, setOrderRaw] = useState<any>();
   const [page, setPage] = useState(1);
-  const [size, setSize] = useState(12);
+  const [size, setSize] = useState(1000);
   const [sort, setSort] = useState(1);
   const sortList = [
     { label: t('common.not_sort'), value: 0 },
