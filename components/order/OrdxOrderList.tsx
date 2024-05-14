b9b486ec1adf6288cb1252defc01086fa1855120
duyouyou <duyouyou88@gmail.com>
2024-05-14 17:15:38 +0800

    order

--- components/order/OrdxOrderList.tsx
+++ components/order/OrdxOrderList.tsx
@@ -214,7 +214,7 @@ export const OrdxOrderList = ({
             ></SortDropdown>
           </div>
         )}
-        <div className="min-h-[30rem] grid  grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-2 sm:gap-4 mb-4">
+        <div className="min-h-[30rem] grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-8 sm:gap-8 mb-4">
           {list.map((item: any, i) => (
             <OrdxFtOrderItem
               showResale={showResale}
