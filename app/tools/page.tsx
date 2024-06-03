68e6baf46196e6f93389fd4c8df10fd94789213e
simonhub2023 <125529150+simonhub2023@users.noreply.github.com>
2024-06-03 11:44:28 +0800

    Add files via upload

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -43,10 +43,10 @@ export default function ToolsPage() {
     },
   ];
   return (
-    <div className="min-h-[10rem] grid  grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-2 sm:gap-4 mt-4 mb-4">
+    <div className="min-h-[10rem] grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-2 sm:gap-4 mt-4 mb-4 pt-2">
       {tools.map((item, i) => (
-        <Card key={i} className="mb-4 ml-4">
-          <CardHeader className="justify-between h-1/4">
+        <Card className="max-w-[340px] mb-4 ml-4" key={i}>
+          <CardHeader className="justify-between">
             <div className="flex gap-5">
               <Avatar name={item.name} className="text-2xl" />
               <div className="flex flex-col gap-1 items-start justify-center">
@@ -56,25 +56,25 @@ export default function ToolsPage() {
               </div>
             </div>
           </CardHeader>
-          <CardBody className="px-3 py-0 text-small justify-center items-center">
+          <CardBody className="px-3 py-0 text-small justify-center items-center pb-8">
             <div className="flex flex-col gap-1 pt-0 items-start justify-center h-30 md:h-20">
               <p className="text-gray-500 pb-4 tracking-[0.06em]">
                 {item.desc}
               </p>
             </div>
-          </CardBody>
-          <CardFooter className="px-3 py-0 text-small justify-center items-center h-1/4 pb-2">
             <WalletConnectBus className="mx-auto mt-1 block">
               <Button
-                //className="inline-block  border-transparent h-7 tracking-[0.2em] text-small line-clamp-1 py-0 bg-primary-300 rounded-lg shadow-lg font-sans uppercase"
-                className="text-tiny md:text-small tracking-[0.25em]  w-35 lg:w-60 bg-gradient-to-r from-indigo-500/50 via-purple-500/50 to-pink-500/50 
+                // className="inline-block  border-transparent h-7 tracking-[0.2em] text-small line-clamp-1 py-0 bg-primary-300 rounded-lg shadow-lg font-sans uppercase"
+                className="text-tiny md:text-small tracking-[0.22em]  w-2/3  bg-gradient-to-r from-indigo-500/50 via-purple-500/50 to-pink-500/50 
                 hover:border-none hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 ${buttonStyles.buyNowButton}` "
+              variant="flat"             
+              radius="sm"
                 onClick={() => clickHandler(item)}
               >
                 {item.btnText}
               </Button>
             </WalletConnectBus>
-          </CardFooter>
+          </CardBody>
         </Card>
       ))}
     </div>
