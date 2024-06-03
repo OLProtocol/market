37524716c5144634687c35e36eb899f7c2c38eba
simonhub2023 <125529150+simonhub2023@users.noreply.github.com>
2024-06-03 11:38:39 +0800

    Add files via upload

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -14,13 +14,13 @@ import { OrdxUtxoList } from '@/components/account/OrdxUtxoList';
 import { OrdxOrderHistoryList } from '@/components/order/OrdxOrderHistoryList';
 import { OrdxOrderList } from '@/components/account/OrdxOrderList';
 import { notification } from 'antd';
-import { getAddressOrdxList } from '@/api';
 import { useReactWalletStore } from 'btc-connect/dist/react';
 import { useTranslation } from 'react-i18next';
 import { OrdxBillList } from '@/components/account/OrdxBillList';
 import { btcToSats, satsToBitcoin } from '@/lib/utils';
 import { Icon } from '@iconify/react';
 import { BtcPrice } from '@/components/BtcPrice';
+import { getAddressOrdxList, getTickerSummary } from '@/api';
 import useSWR from 'swr';
 
 export default function AccountPage() {
@@ -47,19 +47,95 @@ export default function AccountPage() {
   );
   const tickList = useMemo(() => orderListResp?.data || [], [orderListResp]);
 
+  const getPriceByTicker = async (ticker) => {
+    const resp = await getTickerSummary({ ticker });
+    if (resp.code !== 200) {
+      notification.error({
+        message: 'Error',
+        description: resp.msg,
+      });
+      return;
+    }
+    return resp.data.summary.lowest_price;
+  };
   useEffect(() => {
     setTotalSatValue(0);
     let tmp = 0;
     tickList.map(async (item: any) => {
-      // const resp = await getTickerSummary({ ticker: item.ticker });
-      // const price = resp.data.summary.lowest_price;
-      // tmp += Number(price) * item.balance;
-      // setTotalSatValue(totalSatValue + tmp);
+      const resp = await getTickerSummary({ ticker: item.ticker });
+      const price = resp.data.summary.lowest_price;
+      tmp += Number(price) * item.balance;
+      setTotalSatValue(totalSatValue + tmp);
     });
   }, [tickList]);
 
   return (
-    <Tabs
+    <div>
+      <div className="min-h-[8rem] grid  grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-8 gap-2 sm:gap-4 mt-4 mb-4">
+        <Card isHoverable className="px-2">
+          <CardHeader>
+            <span className="text-base md:text-lg font-mono text-gray-400"> 
+              BTC balance
+            </span>
+          </CardHeader>
+          <Divider className='divide-inherit divide-dashed'/>
+          <CardBody className="text-left text-md font-thin leading-8">
+            <div className="flex">
+              <Icon icon="cryptocurrency-color:btc" className="mr-1 mt-2" />
+              {satsToBitcoin(balance.total)}
+            </div>
+            <div className="flex text-sm md:font-bold">
+              <span className="text-yellow-400 w-5"> &nbsp;$</span>
+              <span className="text-gray-400 h-5">
+                <BtcPrice btc={balance.total / 100000000} />
+              </span>
+            </div>
+          </CardBody>
+        </Card>
+
+        <Card isHoverable className="px-2">
+          <CardHeader>
+            <span className="text-base md:text-lg font-mono text-gray-400">
+              Sat20 value
+            </span>
+          </CardHeader>
+          <Divider className='divide-inherit divide-dashed'/>
+          <CardBody className="text-left text-md font-thin leading-8">
+            <div className="flex">
+              <Icon icon="cryptocurrency-color:btc" className="mr-1 mt-2" />
+              {(totalSatValue / 100000000).toFixed(8)}
+            </div>
+            <div className="flex text-sm md:font-bold">
+              <span className="text-yellow-400 w-5"> &nbsp;$</span>
+              <span className="text-gray-400 h-5">
+                <BtcPrice btc={totalSatValue / 100000000} />
+              </span>
+            </div>
+          </CardBody>
+        </Card>
+
+        <Card isHoverable className="px-2">
+          <CardHeader>
+          <span className="text-base md:text-lg font-mono text-gray-400">
+              Ordinals NFT value
+            </span>
+          </CardHeader>
+          <Divider />
+          <CardBody className="text-left text-md font-thin leading-8">
+            <div className="flex">
+              <Icon icon="cryptocurrency-color:btc" className="mr-1 mt-2" />
+              {(totalSatValue / 100000000).toFixed(8)}
+            </div>
+            <div className="flex text-sm md:font-bold">
+              <span className="text-yellow-400 w-5"> &nbsp;$</span>
+              <span className="text-gray-400 h-5">
+                <BtcPrice btc={totalSatValue / 100000000} />
+              </span>
+            </div>
+          </CardBody>
+        </Card>
+      </div>   
+      <Tabs
       aria-label="Options"
       color="primary"
       size="lg"
@@ -74,73 +150,7 @@ export default function AccountPage() {
       style={{ width: '100%' }}
     >
       <Tab key="utxo" title={t('buttons.my_assets')}>
-        <div>
-          <div className="min-h-[8rem] grid  grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-8 gap-2 sm:gap-4 mt-4 mb-4">
-            <Card isHoverable className="px-2">
-              <CardHeader>
-                <span className="text-base md:text-md text-gray-200">
-                  BTC balance
-                </span>
-              </CardHeader>
-              <Divider />
-              <CardBody className="text-left text-md font-thin leading-8">
-                <div className="flex">
-                  <Icon icon="cryptocurrency-color:btc" className="mr-1 mt-2" />
-                  {satsToBitcoin(balance.total)}
-                </div>
-                <div className="flex">
-                  <span className="text-xs text-red-500 w-5">$</span>
-                  <span className="text-yellow-500 text-xs h-5">
-                    <BtcPrice btc={balance.total / 100000000} />
-                  </span>
-                </div>
-              </CardBody>
-            </Card>
-
-            <Card isHoverable className="px-2">
-              <CardHeader>
-                <span className="text-base md:text-md text-gray-200">
-                  Sat20 value
-                </span>
-              </CardHeader>
-              <Divider />
-              <CardBody className="text-left text-md font-thin leading-8">
-                <div className="flex">
-                  <Icon icon="cryptocurrency-color:btc" className="mr-1 mt-2" />
-                  {(totalSatValue / 100000000).toFixed(8)}
-                </div>
-                <div className="flex">
-                  <span className="text-xs text-red-500 w-5">$</span>
-                  <span className="text-yellow-500 text-xs h-5">
-                    <BtcPrice btc={totalSatValue / 100000000} />
-                  </span>
-                </div>
-              </CardBody>
-            </Card>
-
-            <Card isHoverable className="px-2">
-              <CardHeader>
-                <span className="text-base md:text-md text-gray-200">
-                  Ordianls NFT
-                </span>
-              </CardHeader>
-              <Divider />
-              <CardBody className="text-left text-md font-thin leading-8">
-                <div className="flex">
-                  <Icon icon="cryptocurrency-color:btc" className="mr-1 mt-2" />
-                  {(totalSatValue / 100000000).toFixed(8)}
-                </div>
-                <div className="flex">
-                  <span className="text-xs text-red-500 w-5">$</span>
-                  <span className="text-yellow-500 text-xs h-5">
-                    <BtcPrice btc={totalSatValue / 100000000} />
-                  </span>
-                </div>
-              </CardBody>
-            </Card>
-          </div>
-          <OrdxUtxoList />
-        </div>
+        <OrdxUtxoList />
       </Tab>
       <Tab key="history" title={t('common.tx_history')}>
         <OrdxOrderHistoryList address={address} />
@@ -151,6 +161,8 @@ export default function AccountPage() {
       <Tab key="bill" title={t('common.my_biils')}>
         <OrdxBillList />
       </Tab>
-    </Tabs>
+    </Tabs>       
+    </div>
+
   );
 }
