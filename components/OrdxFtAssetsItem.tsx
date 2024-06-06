0ec9c70962b7d4966b2bc5389d452928cdd88089
Hugh <ygz14835187@163.com>
2024-06-06 16:04:26 +0800

    fixed erros

--- components/OrdxFtAssetsItem.tsx
+++ components/OrdxFtAssetsItem.tsx
@@ -12,6 +12,7 @@ import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { hideStr, thousandSeparator } from '@/lib/utils';
 import styles from '@/styles/Orderitem.module.css';
+
 interface Props {
   item: any;
   onSell?: (item: any) => void;
