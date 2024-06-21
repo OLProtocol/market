95f300df34a26827b9cc027a4fa098ccad22dd65
Hugh <ygz14835187@163.com>
2024-06-21 11:41:36 +0800

    chang calc fee

--- components/inscribe/InscribeStepThree.tsx
+++ components/inscribe/InscribeStepThree.tsx
@@ -49,17 +49,17 @@ export const InscribeStepThree = ({
     console.log('type', type);
     console.log('type', list);
     if (['brc20', 'text'].includes(type)) {
-      return 546;
+      return 330;
     } else if (type === 'brc100') {
       return 294;
     } else if (type === 'ordx' && list?.[0]?.op === 'mint') {
       if (list?.[0]?.utxos?.length && list?.[0]?.isSpecial) {
         return list?.[0]?.amount;
       } else {
-        return list?.[0]?.amt > 546 ? list?.[0]?.amt : 546;
+        return list?.[0]?.amt > 330 ? list?.[0]?.amt : 330;
       }
     } else {
-      return 546;
+      return 330;
     }
   }, [type, list]);
   const clacFee = useCalcFee({
