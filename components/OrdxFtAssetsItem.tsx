847edf17124cec0034c2225aba97a6110721a931
simonhub2023 <125529150+simonhub2023@users.noreply.github.com>
2024-06-23 17:29:49 +0800

    Update OrdxFtAssetsItem.tsx

--- components/OrdxFtAssetsItem.tsx
+++ components/OrdxFtAssetsItem.tsx
@@ -44,7 +44,7 @@ export const OrdxFtAssetsItem = ({
   return (
     <Card
       radius="lg"
-      className="forced-colors:hidden border-none w-full h-[14rem] md:h-[18rem] relative hover:border-1 hover:border-solid hover:border-indigo-500 bg-repeat hover:bg-[url('/bg.gif')]"
+      className="card-hover forced-colors:hidden border-none w-[12rem] h-[18rem] md:w-[16rem] md:h-[24rem] relative hover:border hover:border-solid hover:border-indigo-500"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
@@ -60,43 +60,42 @@ export const OrdxFtAssetsItem = ({
           </div>
         </div>
       )}
-      <CardBody className="h-7/10">
-        <div className="flex-1 text-xs tracking-widest antialiased md:text-base uppercase bg-auto bg-left bg-no-repeat bg-[url('/tick/Pearl.png')]">
+      <CardBody className="radius-lg w-[11.5rem] h-[11.75rem] md:w-[15.5rem] md:h-[15.75rem] top-1 bottom-0 left-1">       
+        <div className="flex-1 text-xs tracking-widest antialiased md:text-base uppercase">
+         <div className="absolute inset-0 z-0">
+           <UtxoContent inscriptionId={item?.assets_list[0].inscriptionId} />
+         </div>
+        
           <div className={`label ${isHovered ? 'label-hover' : ''}`}>
             <span className="flex absolute top-2 left-2 text-center text-gray-200">
               {item?.assets_list[0].assets_name}
             </span>
           </div>
-          <div className="flex justify-center relative h-full">
-            <div className="h-full">
-              <UtxoContent
-                inscriptionId={item?.assets_list[0].inscriptionId}
-              ></UtxoContent>
-            </div>
-            <section className="text-center pt-8 absolute top-0 left-0 w-full h-full">
-              <p className="font-medium pt-2 text-2xl md:text-3xl md:pt-3">
-                {thousandSeparator(item?.assets_list[0].amount)}
-              </p>
-              <p className="pt-5">
-                <Snippet
-                  codeString={item?.utxo}
-                  className="bg-transparent text-blue-400"
-                  symbol=""
-                  size="sm"
-                  variant="flat"
-                >
-                  <span className="font-thin md:pl-8">
-                    {hideStr(item?.utxo, 6)}
-                  </span>
-                </Snippet>
-              </p>
-            </section>
-          </div>
+         
         </div>
       </CardBody>
 
-      <CardFooter className="block bg-gray-800 h-3/10">
-        <div className="flex justify-center pb-1">
+      <CardFooter className="block item-center bg-gray-800 w-[12rem] h-[6rem] md:w-[18rem] md:h-[8rem]">          
+          <Snippet
+            codeString={item?.utxo}
+            className="bg-transparent text-blue-400 pt-0 pb-0"
+            symbol=""
+            size="sm"
+            variant="flat"
+          >
+            <span className="font-thin md:pl-8">
+              {hideStr(item?.utxo, 6)}
+            </span>
+          </Snippet>
+
+          <div className='pb-1 md:pb-2'> 
+            {item?.assets_list[0].assets_name} 
+            <span className="relative pl-4 font-medium text-lg md:text-xl">
+               assets: {thousandSeparator(item?.assets_list[0].amount)}
+            </span>
+          
+           </div>
+          <div className="flex item-center pb-1">
           {item.order_id === 0 ? (
             <Button
               // fullWidth
@@ -106,13 +105,13 @@ export const OrdxFtAssetsItem = ({
               // color="primary"
               radius="sm"
               onClick={sellHandler}
-              className="text-tiny h-8 w-full bg-gradient-to-r from-indigo-500/50 via-purple-500/50 to-pink-500/50 hover:border-none hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 ${buttonStyles.buyNowButton}` uppercase"
+              className="text-tiny h-8 w-5/6 bg-gradient-to-r from-indigo-500/50 via-purple-500/50 to-pink-500/50 hover:border-none hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 ${buttonStyles.buyNowButton}` uppercase"
             >
               {t('buttons.list_sale')}
             </Button>
           ) : (
             <Button
-              className="text-tiny h-8 w-full bg-gradient-to-r from-indigo-500/50 via-purple-500/50 to-pink-500/50 hover:border-none hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 ${buttonStyles.buyNowButton}` uppercase"
+              className="text-tiny h-8 w-5/6 bg-gradient-to-r from-indigo-500/50 via-purple-500/50 to-pink-500/50 hover:border-none hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 ${buttonStyles.buyNowButton}` uppercase"
               // fullWidth
               variant="flat"
               // color="default"
