2ad71bbd935a29c5bb840afa154661437f9db8cf
Hugh <ygz14835187@163.com>
2024-07-01 17:40:58 +0800

    add delay

--- components/account/OrdxUtxoTypeList.tsx
+++ components/account/OrdxUtxoTypeList.tsx
@@ -12,44 +12,45 @@ interface OrdxUtxoTypeListProps {
   onChange?: (ticker: string) => void;
 }
 export const OrdxUtxoTypeList = ({ onChange }: OrdxUtxoTypeListProps) => {
-  const router = useRouter();
   const { address, network } = useReactWalletStore((state) => state);
 
   const [selected, setSelected] = useState<string>();
-  const [page, setPage] = useState(1);
-  // const page = useRef(1);
-  const [size, setSize] = useState(12);
 
   const swrKey = useMemo(() => {
-    return `/ordx/getAddressOrdxList-${address}-${network}-${page}-${size}`;
-  }, [address, page, size, network]);
+    return `/ordx/getAddressOrdxList-${address}-${network}`;
+  }, [address, network]);
 
   const { data, isLoading, mutate } = useSWR(
     swrKey,
     () => getAddressOrdxList({ address }),
     {
       revalidateOnFocus: false,
-      // revalidateOnMount: true,
+      revalidateOnReconnect: false,
     },
   );
-  const NsAssetTitle = 'ns';
   const list = useMemo(() => {
     if (!data) {
       return [];
     }
-    let ret = [{assetsName:'Name', assert: 'Name', balance: 0 }];
+    let ret = [{ assetsName: 'Name', assert: 'Name', balance: 0 }];
     for (let i = 0; i < data?.data?.length; i++) {
       const item = data?.data[i];
-      
-      let assert = item?.assets_type + (item?.assets_name ? ':' + item?.assets_name : '');
-      let assetsName = item?.assets_type + (item?.assets_name ? ':' + item?.assets_name : '');
-      if(item?.assets_type === 'ticker'){
-        assetsName = item?.assets_name ? item?.assets_name :'';
+
+      let assert =
+        item?.assets_type + (item?.assets_name ? ':' + item?.assets_name : '');
+      let assetsName =
+        item?.assets_type + (item?.assets_name ? ':' + item?.assets_name : '');
+      if (item?.assets_type === 'ticker') {
+        assetsName = item?.assets_name ? item?.assets_name : '';
       }
       // if(item?.assets_type != 'ticker' && item?.assets_type != 'exotic'){
       //   assetsName =  NsAssetTitle;
       // }
-      ret.push({assetsName: assetsName, assert: assert, balance: item?.balance });
+      ret.push({
+        assetsName: assetsName,
+        assert: assert,
+        balance: item?.balance,
+      });
     }
 
     return ret;
@@ -57,6 +58,7 @@ export const OrdxUtxoTypeList = ({ onChange }: OrdxUtxoTypeListProps) => {
   useEffect(() => {
     console.log('list changed', list);
     if (list.length > 0) {
+      setSelected(list[0].assert);
       onChange?.(list[0].assert);
     }
   }, [list]);
@@ -77,6 +79,7 @@ export const OrdxUtxoTypeList = ({ onChange }: OrdxUtxoTypeListProps) => {
         color="warning"
         radius="full"
         size="lg"
+        selectedKey={selected}
         classNames={{
           tabList: 'flex-wrap rounded-none',
           tab: 'w-min',
