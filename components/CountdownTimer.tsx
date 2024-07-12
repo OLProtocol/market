6770cc0b1f527378872bd5066035e166b94493f9
simonhub2023 <125529150+simonhub2023@users.noreply.github.com>
2024-07-12 10:42:47 +0800

    Add files via upload

--- components/CountdownTimer.tsx
+++ components/CountdownTimer.tsx
@@ -5,32 +5,44 @@ import Countdown from 'react-countdown';
 const CountdownTimer = ({ date }) => {
   const renderer = ({ days, hours, minutes, seconds }) => {
     return (
-      <div className="flex justify-center space-x-4">
-        <div className="text-center">
-          <div className="pt-5 text-4xl h-20 w-20 bg-white/10 rounded-lg">
-            {days}
+       <div className="flex text-2xl md:text-5xl text-purple-600 font-bold justify-center space-x-6">       
+        <div className="relative inline-flex h-12 w-12 mb-3 items-center justify-center rounded-md bg-white/90 p-2 sm:h-16 sm:w-16 md:h-24 md:min-w-[96px] md:min-h-[96px]" >
+          <div className="absolute inset-1 flex flex-col gap-1">
+            <span className="h-full w-full rounded-md bg-primary/10"></span>
+            <span className="h-full w-full rounded-md bg-white"></span>
           </div>
-          <div>Days</div>
-        </div>
-        <div className="text-center">
-          <div className="pt-5 text-4xl h-20 w-20 bg-white/10 rounded-lg">
-            {hours}
+          <span className="relative">{days}</span>
+          <span className="absolute bottom-[-2.2rem] text-gray-300 text-sm md:text-xl">Days</span>
+        </div >
+        
+        <span className='pt-1 md:pt-6'>:</span>
+        <div className="relative inline-flex h-12 w-12 mb-3 items-center justify-center rounded-md bg-white/90 p-2 sm:h-16 sm:w-16 md:h-24 md:min-w-[96px] md:min-h-[96px]">
+          <div className="absolute inset-1 flex flex-col gap-1">
+            <span className="h-full w-full rounded-md bg-primary/10"></span>
+            <span className="h-full w-full rounded-md bg-white"></span>
           </div>
-          <div>Hours</div>
+          <span className="relative"> {hours}</span>
+          <span className="absolute bottom-[-2.2rem] text-gray-300 text-sm md:text-xl">Hours</span>
         </div>
-        <div className="text-center">
-          <div className="pt-5 text-4xl h-20 w-20 bg-white/10 rounded-lg">
-            {minutes}
+        <span className='pt-1 md:pt-6'>:</span>
+        <div className="relative inline-flex h-12 w-12 mb-3 items-center justify-center rounded-md bg-white/90 p-2 sm:h-16 sm:w-16 md:h-24 md:min-w-[96px] md:min-h-[96px]">
+          <div className="absolute inset-1 flex flex-col gap-1">
+            <span className="h-full w-full rounded-md bg-primary/10"></span>
+            <span className="h-full w-full rounded-md bg-white"></span>
           </div>
-          <div>Minutes</div>
+          <span className="relative">{minutes}</span>
+          <span className="absolute bottom-[-2.2rem] text-gray-200 text-sm md:text-xl">Minutes</span>
         </div>
-        <div className="text-center">
-          <div className="pt-5 text-4xl h-20 w-20 bg-white/10 rounded-lg">
-            {seconds}
+        <span className='pt-1 md:pt-6'>:</span>
+        <div className="relative inline-flex h-12 w-12 mb-3 items-center justify-center rounded-md bg-white/90 p-2 sm:h-16 sm:w-16 md:h-24 md:min-w-[96px] md:min-h-[96px]">
+          <div className="absolute inset-1 flex flex-col gap-1">
+            <span className="h-full w-full rounded-md bg-primary/10"></span>
+            <span className="h-full w-full rounded-md bg-white"></span>
           </div>
-          <div>Seconds</div>
+          <span className="relative">{seconds}</span>
+          <span className="absolute bottom-[-2.2rem] text-gray-300 text-sm md:text-xl">Seconds</span>
         </div>
-      </div>
+      </div >
     );
   };
 
