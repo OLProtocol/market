43b0623b8ff4398c8b532c7a939a1deab2066492
Hugh <ygz14835187@163.com>
2024-07-17 21:57:53 +0800

    hide one utxo

--- components/inscribe/InscribeStepThree.tsx
+++ components/inscribe/InscribeStepThree.tsx
@@ -296,14 +296,14 @@ export const InscribeStepThree = ({
           </div>
           {files.length > 1 && (
             <>
-              <div className="mb-4">
+              {/* <div className="mb-4">
                 <Checkbox
                   isSelected={selected}
                   onValueChange={(value) => setSelected(value)}
                 >
                   {t('pages.inscribe.step_three.output_one_utxo')}
                 </Checkbox>
-              </div>
+              </div> */}
               {/* <div className="mb-4">
                 <Checkbox
                   isSelected={tightSelected}
