5777c6febbe1eea9c535157490134f9cd4bb0971
simonhub2023 <125529150+simonhub2023@users.noreply.github.com>
2024-07-23 16:29:27 +0800

    Update BannerTop.tsx

--- components/home/BannerTop.tsx
+++ components/home/BannerTop.tsx
@@ -10,8 +10,8 @@ import ProgressBar from '@/components/ProgressBar';
 const BannerTop = ({ detail }: any) => {
   const router = useRouter();
   const today = new Date();
-  const startDate = new Date('2024-07-02T03:58:00');
-  const targetDate = new Date('2024-07-22T19:05:00');
+  const startDate = new Date('2024-07-23T03:58:00');
+  const targetDate = new Date('2024-08-10T00:00:00');
   const { data: heightData } = ordxSWR.useBtcHeight('livenet');
   const formatDate = (date) => {
     return date.toLocaleDateString('en-US', {
@@ -22,28 +22,38 @@ const BannerTop = ({ detail }: any) => {
   };
 
   const toMint = () => {
-    router.push('/inscribe?type=ordx&ticker=RarePizza');
+    router.push('/inscribe?type=name&ticker=');
   };
   const currentBlockHeight = useMemo(() => {
     return heightData?.data.height || 0;
   }, [heightData]);
-  const endBlockHeight = 853358;
+  const startBlockHeight = 853472;
+  const endBlockHeight = 856000;
   return (
     <section className="bg-[#060818] text-white md:py-8 md:px-8 rounded-lg">
       <div className="mx-auto rounded-lg">
         <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
           <div>
-            <div>
-              <div className="mt-1 md:mt-0 mb-4 text-ellipsis text-lg text-gray-400 bg-transparent font-bold">
+            <div className="pl-10 md:pl-30 justify-center items-center">
+              <div className="mt-1 md:mt-0 mb-4 text-ellipsis text-lg text-orange-500 bg-transparent font-bold">
                 {formatDate(today)}
               </div>
-              <h2 className="text-3xl md:text-5xl font-bold mb-4 mt-14">
-                🔥 RarePizza is now <br />
-                <span className="text-blue-500">being hotly minted!</span>
-              </h2>
-              <p className="mb-8 text-xs md:text-xl text-yellow-400">
-                Efficient, Cost-effective, and Time-saving Minting.
-              </p>
+              <div>
+              <Image
+                radius="full"
+                src="./names-free.png"
+                alt="logo"
+                className="w-1/2 h-1/2 p-1 rounded-full"
+              />
+
+              </div>
+              {/* <h2 className="text-3xl md:text-7xl font-bold mb-4 mt-14">
+                🔥 Names is now <br />
+                <span className="text-blue-500">being free minted!</span>
+              </h2> */}
+              <div className="mb-8 text-sm md:text-xl text-yellow-400">
+                Supports suffixes: for example, .btc, .x, .pizza, and no suffix.
+              </div>
               <div>
                 <Button
                   className="text-sm font-blod h-12 w-60 md:w-96 bg-gradient-to-r from-indigo-500/50 via-purple-500/50 to-pink-500/50 hover:border-none hover:to-indigo-500 hover:via-purple-500 hover:from-pink-500 uppercase"
@@ -51,7 +61,7 @@ const BannerTop = ({ detail }: any) => {
                   radius="sm"
                   onClick={toMint}
                 >
-                  <span className="md:text-2xl">🍕</span> MINT NOW
+                  <span className="md:text-2xl">🍕</span> Free Mint
                 </Button>
               </div>
             </div>
@@ -60,13 +70,13 @@ const BannerTop = ({ detail }: any) => {
             <div className="bg-gray-800/50 p-4  text-xs rounded-lg">
               <div className="text-center">
                 <h3 className="text-2xl md:text-4xl font-bold text-primary mb-4 uppercase">
-                  RarePizza Mint End In
+                  Names Free Mint End In
                 </h3>
                 <CountdownTimer date={targetDate} />
                 <div className="mt-8">
                   <div className="flex justify-between mb-2">
                     <h6>
-                      <span>{detail?.startBlock?.toLocaleString()} </span>
+                      <span>{startBlockHeight?.toLocaleString()} </span>
                     </h6>
                     <h6 className="pr-30 keep-all">
                       Current:{' '}
@@ -75,19 +85,19 @@ const BannerTop = ({ detail }: any) => {
                       </span>
                     </h6>
                     <h6>
-                      <span>{detail?.endBlock?.toLocaleString()} </span>
+                      <span>{endBlockHeight?.toLocaleString()} </span>
                     </h6>
                   </div>
                   <ProgressBar
-                    start={detail?.startBlock}
+                    start={startBlockHeight}
                     current={currentBlockHeight}
-                    target={detail?.endBlock}
+                    target={endBlockHeight}
                     isdisplay={true}
                   />
                   <div className="flex justify-between mt-2">
                     <p className="fs-14">{formatDate(startDate)}</p>
                     <p className="fs-14">
-                      <h6 className="pr-30 keep-all">
+                      {/* <h6 className="pr-30 keep-all">
                         Holders:{' '}
                         <span className="text-green-500">
                           {thousandSeparator(detail?.holdersCount)}
@@ -97,7 +107,7 @@ const BannerTop = ({ detail }: any) => {
                         <span className="text-green-500">
                           {thousandSeparator(detail?.totalMinted)}
                         </span>
-                      </h6>
+                      </h6> */}
                     </p>
                     <p className="fs-14"> {formatDate(targetDate)}</p>
                   </div>
