e780fbb191de507260168870ee652b15401b63f8
Hugh <ygz14835187@163.com>
2024-07-31 18:13:26 +0800

    add order

--- lib/utils/order.ts
+++ lib/utils/order.ts
@@ -337,6 +337,59 @@ export const buildBuyOrder = async ({
   return rawTxHex;
 };
 
+export const generateBuyInputsAndOutputs = async ({
+  raws,
+  dummyUtxos,
+}: any) => {
+  const { network, address } = useReactWalletStore.getState();
+  const psbtNetwork = toPsbtNetwork(
+    network === 'testnet' ? NetworkType.TESTNET : NetworkType.MAINNET,
+  );
+  const dummyInputs: any[] = dummyUtxos.map((v) => {
+    return {
+      hash: v.txid,
+      index: v.vout,
+      witnessUtxo: {
+        script: Buffer.from(addresToScriptPublicKey(address), 'hex'),
+        value: v.value,
+      },
+      sighashType: bitcoin.Transaction.SIGHASH_ALL,
+    };
+  });
+  const sellInputs: PsbtInput[] = [];
+  const sellOutputs: any[] = [];
+  const buyOutputs: any[] = [];
+  for (let i = 0; i < raws.length; i++) {
+    const raw = raws[i];
+    const sellPsbt = bitcoin.Psbt.fromHex(raw, {
+      network: psbtNetwork,
+    });
+
+    const sellerInput = {
+      hash: sellPsbt.txInputs[0].hash as any,
+      index: sellPsbt.txInputs[0].index,
+      witnessUtxo: sellPsbt.data.inputs[0].witnessUtxo as any,
+      finalScriptWitness: sellPsbt.data.inputs[0].finalScriptWitness,
+    };
+
+    sellInputs.push(sellerInput);
+    const ordValue = sellPsbt.data.inputs[0].witnessUtxo!.value;
+    const ordOutput = {
+      address,
+      value: ordValue,
+    };
+    sellOutputs.push(ordOutput);
+    const sellerOutput = sellPsbt.txOutputs[0];
+    buyOutputs.push(sellerOutput);
+  }
+  const inputs = [...sellInputs];
+  const outputs = [...sellOutputs, ...buyOutputs];
+  return {
+    inputs,
+    outputs,
+    dummyInputs,
+  };
+};
 export const calcBuyOrderFee = async ({
   orders,
   utxos,
