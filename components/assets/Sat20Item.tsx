ce7fa7fa7edcec5e348c0f9c14ea4ba55a585ac4
Hugh <ygz14835187@163.com>
2024-08-04 19:05:58 +0800

    chagne ui

--- components/assets/Sat20Item.tsx
+++ components/assets/Sat20Item.tsx
@@ -19,11 +19,11 @@ export const Sat20Item = ({ asset }: Props) => {
       <div className="left-0 top-0 flex z-10 absolute p-2 rounded-br-[1rem] text-center text-gray-200 bg-gradient-to-r from-indigo-500/50 via-purple-500/50 to-pink-500/50 backdrop-saturate-50 hover:text-gray-100">
         {asset?.assets_name}
       </div>
-      <section className="text-center pt-10 font-mono md:pt-12 absolute top-0 left-0 w-full h-full z-10">
+      <section className="text-center pt-4 font-mono md:pt-12 absolute top-0 left-0 w-full h-full z-10">
         <p className="font-medium pt-3 text-2xl md:text-3xl md:pt-3">
           {thousandSeparator(asset?.amount)}
         </p>
-        <p className="pt-12 md:pb-2 md:text-xs p-4">
+        <p className=" pt-4 md:pt-12 md:pb-2 md:text-xs p-4">
           <span className="font-mono text-gray-100">{tickContent}</span>
         </p>
       </section>
