9f03152b52fc6f8520eb893b5f5ecf1a4a4612b2
Hugh <ygz14835187@163.com>
2024-08-18 19:11:07 +0800

    add notice

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,7 +79,7 @@ export default function RootLayout({
           <div className="relative flex flex-col h-screen">
             {/* <ParticleEffect /> Add the ParticleEffect component here */}
             <Navbar />
-            <SystemNoticeModal />
+            {/* <SystemNoticeModal /> */}
             <main className="mx-auto w-full px-2 md:px-4 flex-grow">
               {children}
             </main>
--- assets/version.js
+++ assets/version.js
@@ -1,1 +1,1 @@
-export const version = 91;
+export const version = 92;
