7c473dd2d241d3fe67289c2a61a2a85f46f12b8a
softwarecheng <softwarecheng@126.com>
2024-09-09 08:46:01 +0800

    add AUTHORIZATION

--- api/ordx.ts
+++ api/ordx.ts
@@ -7,6 +7,9 @@
  */
 import axios from 'axios';
 import mempool from './mempool';
+
+// axios.defaults.headers.common['Authorization'] = process.env.NEXT_PUBLIC_ORDX_API_AUTHORIZATION;
+
 const generateUrl = (url: string, network?: string) => {
   url = `${process.env.NEXT_PUBLIC_ORDX_HOST}${network === 'testnet' ? '/testnet4' : '/mainnet'}/${url}`;
   console.log('hostname', location.hostname);
