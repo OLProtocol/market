b61c2612bfe1cf02ecd5a138648fc15b672325f4
simonhub2023 <125529150+simonhub2023@users.noreply.github.com>
2024-09-09 17:11:26 +0800

    new home

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
     { from: 'public/raresats', to: 'out/raresats' },
     { from: 'public/icon', to: 'out/icon' },
   ],
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
